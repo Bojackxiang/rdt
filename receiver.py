547060791149cf0f3ac10ab322e8fe5050b29ac2
iambatman <batmandocode@gmail.com>
2017-09-09 12:43:25 +1000

    problem: sending is always waiting for something...

--- receiver.py
+++ receiver.py
@@ -3,51 +3,83 @@ import sys
 
 
 # Networking Setting
-host = ''
-port = 2052
+host = 'localhost'
+port = 3102
 address = (host, port)
 
 
 
-# Socket Programing Initialization
-upd_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+# Hand Shake Start here
+# Receive Data From Sender
+upd_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 upd_socket.bind((address))
-upd_socket.listen(1)
-conn, addr = upd_socket.accept()
+# Trying To Receive Data Countinusous
+
+while True:
+    print("accepting data")
+    data, addr = upd_socket.recvfrom(1024)
+    # byte data to string data
+    print(data.decode('utf-8'))
+    if data.decode('utf-8') == "10":
+        print("there is a 10")
+        break
+    upd_socket.sendto("back message".encode("UTF-8"), (address))
+    print("sending data")
+    data, addr = upd_socket.recvfrom(1024)
+    print("it reach end")
+    break
+
+    # number = data.decode("UTF-8")
+
+
+
+
+
+# receive data from sender, add 1 to it and send back the total sum
+
+# receive 1024 byte at a time
+# number = int(data.decoded("UTF-8")) + 1
+# print("I just received a data" + data)
+# data = str(number)
+# print("now i want to send a data", data)
+# upd_socket.send(data.encode('UTF-8'))
+
 
 
 
 # Prepare for receive data
-content = ""
+# content = ""
 
 
 
 # Receive Data From Sender
-while True:
-    # The Content of Received Data
-    data = conn.recv(1024)
-    if not data:
-        print("There is no data come")
-        break
-    else:
-        # Transfer Bytes File To String
-        content += data.decode('utf-8')
-        # TRY: receiver send a data to sender
-        conn.send(data)
-        break
+# while True:
+#     # The Content of Received Data
+#     # data = conn.recv(1024)
+#     if not data:
+#         print("There is no data come")
+#         break
+#     else:
+#         # Transfer Bytes File To String
+#         content += data.decode('utf-8')
+#         # TRY: receiver send a data to sender
+#         conn.send(data)
+#         break
 
 
 
 # Create a File For Content
 # path = "/Users/keiichi/Desktop/9331_project/data.txt"
-file = open("data.txt", "w")
-file.write(content)
-file.close()
+# file = open("data.txt", "w")
+# file.write(content)
+# file.close()
 
 
 
 # CLOSE CONNECTION
-conn.close()
+upd_socket.close()
 
 
 
--- sender.py
+++ sender.py
@@ -2,34 +2,86 @@ import socket
 import sys
 
 # Reading Target File
-filePath = sys.argv[3]
-filePath = "/Users/keiichi/Desktop/9331_project/content/9331txt1.txt"
-print(filePath)
+# filePath = sys.argv[3]
+# filePath = "/Users/keiichi/Desktop/9331_project/content/9331txt1.txt"
+# print(filePath)
 
 
 
 # # Reading File and add to content
-content = ""
-with open(filePath) as file:
-    for line in file:
-        content += line
+# content = ""
+# with open(filePath) as file:
+#     for line in file:
+#         content += line
 
 
 
-# # Networking Setting
-host = sys.argv[1]
-port = int(sys.argv[2])
-client_socket = socket.socket()
-client_socket.connect((host, port))
+# # Networking Setting (SUCCESS)
+# host = sys.argv[1]
+# port = int(sys.argv[2])
+host = "localhost"
+port = 3102
+addr = (host, port)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# client_socket.bind(addr)
+mss = 10
+# log_file = open("log_file.txt")
+
+
+
+# Three Way Handshake Start here
+def three_way_handshake(host, port):
+    # s : SYN -> R:
+    # r : SYN, ACK -> s
+    # s : ACK -> Receiver
+    # Done
+    syn_number = 200
+    ack_number = 0
+    fin_number = 0
+    new_syn_number = 0
+    new_ack_number = 0
+
+    # send data to receiver
+    print("sending data")
+    client_socket.sendto(str(syn_number).encode("UTF-8"), (host, port))
+
+    print("receiving data")
+    data, addr = client_socket.recv(1024)
+    print("the receving data is "+ data)
+
+three_way_handshake(host, port)
+
+
+
+
+
+# only send a number to receiver at the beginning (SUCCESS)
+# data = 20
+# client_socket.sendto(data.encode("UTF-8"), addr)
+# print("I just send a data to receiver")
+
+
+
+
+
+#Trying To Receive Data From Sender (TESTING)
+# data, addr = client_socket.recvfrom(1024)
+# print(data)
+
 
 
 
 # Receving Any FeedBack From Receiver
-while content:
-    client_socket.send(content.encode('utf-8'))
-    # It can receive the file send from Receiver (In Byte Type)
-    data = client_socket.recv(1024)
-    break
+# while content:
+    # client_socket.send(content.encode('utf-8'))
+    # # It can receive the file send from Receiver (In Byte Type)
+    # data = client_socket.recv(1024)
+    # break
+
+
+
+
+
 client_socket.close()
 
 
